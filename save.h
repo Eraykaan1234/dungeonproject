d15eb4a4f2ec099ad90599b47f2b08a2433367ea
erayK <eraykaan55@outlook.com>
2025-05-26 21:40:48 +0200

    save.h/ save_game() / load_game() veranderd

--- save.h
+++ save.h
@@ -1,10 +1,10 @@
 #ifndef SAVE_H
 #define SAVE_H
 
-#include "dungeon.h"
 #include "player.h"
+#include "dungeon.h"
 
-void save_game(Room **rooms, int count, Player *player);
-void load_game(Room ***rooms, int *count, Player *player);
+void save_game(const char *filename, Player *player, Room **rooms, int room_count);
+void load_game(const char *filename, Player *player, Room ***rooms_ptr, int *room_count_ptr);
 
 #endif
